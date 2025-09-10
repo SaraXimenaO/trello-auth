7ee90cecba661b560fc1fc6ad207ec22a1158a25
Sara Ximena Ortiz Reyes <xreyes596@gmail.com>
2025-09-10 16:46:52 -0500

    Laboratorio auth

--- src/app/modules/auth/components/login-form/login-form.component.ts
+++ src/app/modules/auth/components/login-form/login-form.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router , ActivatedRoute} from '@angular/router';
 import { faPen, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { RequestStatus } from '@models/request-status.model';
 
+import { AuthService } from '@services/auth.service'
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html'
@@ -17,18 +19,39 @@ export class LoginFormComponent {
   faEye = faEye;
   faEyeSlash = faEyeSlash;
   showPassword = false;
-  status: string = 'init';
+  status: RequestStatus = 'init';
 
   constructor(
     private formBuilder: FormBuilder,
-    private router: Router
-  ) { }
+    private router: Router,
+    private route: ActivatedRoute,
+    private authService: AuthService
+  ) {
+    this.route.queryParamMap.subscribe(params => {
+      const email = params.get('email');
+      if(email){
+        this.form.controls.email.setValue(email);
+      }
+    })
+  }
 
   doLogin() {
     if (this.form.valid) {
       this.status = 'loading';
       const { email, password } = this.form.getRawValue();
-      // TODO
+      this.authService.login(email, password)
+      .subscribe(
+        {
+          next: () => {
+            this.status='success';
+            this.router.navigate(['/app']);
+
+          },
+          error: () => {
+            this.status='failed';
+          }
+        }
+      );
     } else {
       this.form.markAllAsTouched();
     }
--- src/app/modules/auth/components/register-form/register-form.component.ts
+++ src/app/modules/auth/components/register-form/register-form.component.ts
@@ -3,36 +3,84 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
+
 import { CustomValidators } from '@utils/validators';
+import { RequestStatus } from '@models/request-status.model';
+import { AuthService} from '@services/auth.service'
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
 })
 export class RegisterFormComponent {
+
+  formValidateUser = this.formBuilder.nonNullable.group({
+    email: ['', [Validators.email, Validators.required]]
+  });
+
   form = this.formBuilder.nonNullable.group({
     name: ['', [Validators.required]],
     email: ['', [Validators.email, Validators.required]],
-    password: ['', [Validators.minLength(6), Validators.required]],
+    password: ['', [Validators.minLength(8), Validators.required]],
     confirmPassword: ['', [Validators.required]],
   }, {
     validators: [ CustomValidators.MatchValidator('password', 'confirmPassword') ]
   });
-  status: string = 'init';
+  status: RequestStatus = 'init';
+  statusValidateUser : RequestStatus = 'init';
   faEye = faEye;
   faEyeSlash = faEyeSlash;
   showPassword = false;
+  showRegister = false;
 
   constructor(
     private formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) {}
 
   register() {
     if (this.form.valid) {
       this.status = 'loading';
       const { name, email, password } = this.form.getRawValue();
+      this.authService.Register(name, email, password)
+      .subscribe({
+        next: () => {
+          this.status = 'success';
+          this.router.navigate(['/login']);
+        },
+        error: () =>{
+          this.status = 'failed';
+        }
+      });
       console.log(name, email, password);
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+
+  validateUser(){
+    if (this.formValidateUser.valid) {
+      this.statusValidateUser = 'loading';
+      const { email } = this.formValidateUser.getRawValue();
+      this.authService.ValidateUser( email)
+      .subscribe({
+        next: (result) => {
+          if(result.isAvailable){
+            this.statusValidateUser = 'success';
+            this.form.controls.email.setValue(email);
+            this.showRegister = true;
+
+          }else{
+            this.router.navigate(['/login'],{
+              queryParams: {email}
+            });
+          }
+        },
+        error: () =>{
+          this.status = 'failed';
+        }
+      });
     } else {
       this.form.markAllAsTouched();
     }
