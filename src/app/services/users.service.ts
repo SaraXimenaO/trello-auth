8932ac4feb8104621267f258f0247559bdcd5171
Sara Ximena Ortiz Reyes <xreyes596@gmail.com>
2025-09-15 17:40:31 -0500

    interceptor para agregar token a las peticiones

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -3,6 +3,7 @@ import { HttpClient} from '@angular/common/http';
 import {environment} from '@environments/environment';
 import {TokenService} from '@services/token.service';
 import { User } from '@models/user.model';
+import { checkToken } from '@interceptors/token.interceptor';
 
 @Injectable({
   providedIn: 'root'
@@ -17,9 +18,7 @@ export class UsersService {
   getUsers(){
     const token = this.tokenService.getToken();
     return this.http.get<User[]>(`${this.apiUrl}/api/v1/users`,{
-      headers:{
-        Authorization: `Bearer ${token}`
-      }
+      context: checkToken()
     });
   }
 }
